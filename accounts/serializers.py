16cfd739dd71491973fb42aa72a3c5a29b834d21
malek <Abdelmalekbenayadcherif@gmail.com>
2025-12-12 23:56:03 +0100

    Add project source code

---
+++ accounts/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import Compte
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class StudentRegSerializer(serializers.ModelSerializer):
+    # serializer pour l'inscription
+    
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Compte
+        fields = ['id', 'email', 'password', 'nom', 'prenom']
+
+    def create(self, validated_data):
+        # print("DEBUG: creating user")
+        print(validated_data) # pour verifier les données
+
+        # On fait à la main pour être sur
+        password = validated_data.pop('password')
+        user = Compte(**validated_data)
+        user.set_password(password)
+        user.role = "STUDENT" # on force le role ici
+        user.save()
+        return user
+
+
+class CustomTokenSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Ajout des trucs dans le token
+        token['email'] = user.email
+        token['role'] = user.role
+        token['nom'] = user.nom
+        token['prenom'] = user.prenom
+        
+        # print(token) 
+
+        return token
